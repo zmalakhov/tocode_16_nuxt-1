e1e15ec711e836c53812db5c7e52ff3447a471f4
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-09 14:48:13 +0300

    #2 - установка, #4 - Динамические роуты

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>post id is: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+  export default {
+    validate({params}) {
+      // Number
+      return /^\d+$/.test(params.id)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="posts">
+    <input type="text" v-model="postId">
+    <button @click="loadPost">Get Post</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        postId: ''
+      }
+    },
+    methods:{
+      loadPost(){
+        this.$router.push(`/posts/${this.postId}`)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
