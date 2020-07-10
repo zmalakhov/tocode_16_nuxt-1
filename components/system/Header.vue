a45b69e2e2cddec67b1e764b38602c90032bd791
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-10 18:15:47 +0300

    #15 - Errors - layout - Обработка ошибок

--- components/system/Header.vue
+++ components/system/Header.vue
@@ -3,7 +3,9 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <div class="logo">Blog</div>
+          <div class="logo">
+            <nuxt-link to="/">Blog</nuxt-link>
+          </div>
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.title">
               <nuxt-link class="navbar-link"
@@ -18,21 +20,25 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        links: [
-          {title: 'Home', url: '/'},
-          {title: 'About', url: '/about'},
-        ]
-      }
+    export default {
+        data() {
+            return {
+                links: [
+                    {title: 'Blog', url: '/blog'},
+                    {title: 'About', url: '/about'},
+                ]
+            }
+        }
     }
-  }
 </script>
 
 <style lang="scss">
-  .navbar-link{
-    &.nuxt-link-exact-active{
+  .logo a {
+    color: #333333;
+  }
+
+  .navbar-link {
+    &.nuxt-link-exact-active {
       color: #4165de;
     }
   }
---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="not-found">
+    <div class="container">
+      <div class="not-found__wrapper">
+        <p class="title"> Page Not Found </p>
+        <nuxt-link to="/"> Back to main page </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "Error"
+    }
+</script>
+
+<style lang="scss">
+  .not-found__wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    height: 80vh;
+  }
+</style>
