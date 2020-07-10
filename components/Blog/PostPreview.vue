d7d6d2620fa73873f9632df9b66f0b45edd230a2
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-10 11:21:08 +0300

    #8 - Выводим посты - Посты на главной странице

---
+++ components/Blog/PostPreview.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post-preview">
+    <img :src="post.img" :alt="post.title">
+    <div class="post-content">
+      <h3 class="title">{{ post.title }}</h3>
+      <p>{{ post.descr }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      post: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .post-preview {
+    width: 33%;
+    padding: 16px;
+    margin-bottom: 20px;
+    text-align: center;
+
+    img {
+      margin-bottom: 16px;
+    }
+
+    .post-content {
+      p{
+        color: #999999;
+      }
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,42 @@
     <promo/>
     <intro title="My last posts:"/>
     <!--posts-->
+    <postsList :posts="posts"></postsList>
   </div>
 </template>
 
 <script>
   import promo from '@/components/Promo.vue'
   import intro from '@/components/UI/Intro.vue'
+  import postsList from '@/components/Blog/PostsList.vue'
 
   export default {
     components: {
-      promo, intro
+      promo, intro, postsList
+    },
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            title: '1 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+          },
+          {
+            id: 2,
+            title: '2 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg',
+          },
+          {
+            id: 3,
+            title: '3 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://dsvf96nw4ftce.cloudfront.net/images/detailed/2/extra-tall-maxi-pet-gate.jpg?t=1527097331',
+          },
+        ]
+      }
     }
   }
 </script>
