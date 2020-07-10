9e11d6b9e39f925bcd3c28546fcf9044a41c3a0a
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-10 12:32:12 +0300

    #10 - Единичный пост

---
+++ pages/blog/_id/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="post">
+      <div class="container">
+
+        <!--header-->
+        <div class="post-header">
+          <img :src="post.img" :alt="post.title">
+          <h1 class="title"> {{ post.title }} </h1>
+          <p> {{ post.descr }} </p>
+        </div>
+
+        <!--body-->
+        <div class="post-body">
+          <p> {{ post.content }} </p>
+        </div>
+
+      </div>
+    </section>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        post: {
+          id: 1,
+          title: '1 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.post{
+  max-width: 900px;
+  margin: 0 auto;
+}
+  .post-header{
+    text-align: center;
+    margin-bottom: 30px;
+    img{
+      margin-bottom: 16px;
+      max-width: 400px;
+    }
+    p{
+      color: #999999;
+    }
+  }
+  .post-content{
+    text-align: left;
+  }
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <intro title="My posts:"/>
+    <postsList :posts="posts"></postsList>
+  </div>
+</template>
+
+<script>
+  import intro from '@/components/UI/Intro.vue'
+  import postsList from '@/components/Blog/PostsList.vue'
+
+  export default {
+    components: {
+      intro, postsList,
+    },
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            title: '1 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+          },
+          {
+            id: 2,
+            title: '2 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg',
+          },
+          {
+            id: 3,
+            title: '3 post',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            img: 'https://dsvf96nw4ftce.cloudfront.net/images/detailed/2/extra-tall-maxi-pet-gate.jpg?t=1527097331',
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style></style>
