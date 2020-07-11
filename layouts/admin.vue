f15d156b46a2164d064f5bf1f2b8959990c31f4b
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-11 09:34:36 +0300

    #17 - Админ панель - Базис + layout

---
+++ layouts/admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Header/>
+    <div class="wrapper-content wrapper-content--fixed">
+      <Intro title="Admin Page">
+        <nuxt-link to="/admin" class="link linkWhite"> Admin</nuxt-link>
+      </Intro>
+      <Nuxt/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    import Header from "@/components/system/Header";
+
+    export default {
+        components: {Header}
+    }
+</script>
+
