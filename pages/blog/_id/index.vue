db462af1bfa2fac75810bb83619aeeb695495635
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-14 13:22:33 +0300

    2#8 - Вывод единичного поста - Вывод единичного поста и подгрузка всех комментариев

--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -8,27 +8,40 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   import post from '@/components/Blog/Post.vue'
   import newComment from '@/components/Comments/NewComment.vue'
   import comments from '@/components/Comments/Comments.vue'
+
   export default {
     components:{ post, comments, newComment },
-    data() {
+    async asyncData(context) {
+      let [post, comments] = await Promise.all([
+        axios.get(`https://blog-nuxt-7d8fd.firebaseio.com/posts/${context.params.id}.json`),
+        axios.get(`https://blog-nuxt-7d8fd.firebaseio.com/comments.json`)
+      ])
       return {
-        post: {
-          id: 1,
-          title: '1 post',
-          descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-          img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
-        },
-        comments: [
-          {name: 'Alex', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
-          {name: 'Nick', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
-          {name: 'John', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
-        ]
+        post: post.data,
+        comments: comments.data,
       }
     }
+    // data() {
+    //   return {
+    //     post: {
+    //       id: 1,
+    //       title: '1 post',
+    //       descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //       img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+    //     },
+    //     comments: [
+    //       {name: 'Alex', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
+    //       {name: 'Nick', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
+    //       {name: 'John', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'},
+    //     ]
+    //   }
+    // }
   }
 </script>
 
