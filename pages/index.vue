2678f9693945112bfcb58b800ae29b3d052ed31c
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-14 09:04:25 +0300

    2#2 - Про asycData - Работа с асинхронными данными

--- pages/index.vue
+++ pages/index.vue
@@ -16,30 +16,42 @@
     components: {
       promo, contacts
     },
-    data() {
-      return {
-        posts: [
-          {
-            id: 1,
-            title: '1 post',
-            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
-          },
-          {
-            id: 2,
-            title: '2 post',
-            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg',
-          },
-          {
-            id: 3,
-            title: '3 post',
-            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            img: 'https://dsvf96nw4ftce.cloudfront.net/images/detailed/2/extra-tall-maxi-pet-gate.jpg?t=1527097331',
-          },
-        ]
-      }
+    asyncData(context){
+      return new Promise((resolve,reject)=>{
+
+        setTimeout(() => {
+          resolve({
+            posts: [
+              {
+                id: 1,
+                title: '1 post',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+              }
+            ]
+          })
+        }, 1500)
+
+      })
+      .then( data => {
+        return data
+      })
+      .catch(e => {
+        context.error(e)
+      })
     }
+    // data() {
+    //   return {
+    //     posts: [
+    //       {
+    //         id: 1,
+    //         title: '1 post',
+    //         descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+    //       }
+    //     ]
+    //   }
+    // }
   }
 </script>
 
